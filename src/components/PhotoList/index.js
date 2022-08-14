876fd353dad437a6838cb7ffcef9b3b2455c65fe
lotus <daniel.younghwan.lee@gmail.com>
2022-08-14 12:56:18 -0400

    tests finished

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -109,7 +109,7 @@ function PhotoList({ category }) {
             <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                     <img
-                        src={require(`../../assets/small/${category}/${i}.jpg`).default}
+                        src={require(`../../assets/small/${category}/${i}.jpg`)}
                         alt={image.name}
                         className="img-thumbnail mx-1"
                         key={image.name}

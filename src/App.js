77a931157586560a2bbe54fcb19f705fd83aa573
lotus <daniel.younghwan.lee@gmail.com>
2022-08-14 20:01:12 -0400

    form submission

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
-
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -29,6 +29,7 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
       currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
